09b5d3daf4f4a4f484056d94f62b47d28910207e
belialdev <belialdev@anim.belialdev>
2013-04-10 23:16:28 -0400

    Initial commit - complete stock Galaxy Reverb kernel source from latest update and Samsung opensource release center SPH-M950

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Wed Apr 10 19:50:34 EDT 2013"
+#define LINUX_COMPILE_BY "belialdev"
+#define LINUX_COMPILE_HOST "anim.belialdev"
+#define LINUX_COMPILER "gcc version 4.7.3 20130226 (prerelease) (crosstool-NG hg+default-cbaf37cc20b7 - Linaro GCC 2013.03 - Cortex-A8) "
